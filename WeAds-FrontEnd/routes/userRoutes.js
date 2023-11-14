3ee149bc14af639fe69f63b6ccf0911188dca36b
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-14 22:15:07 +0700

    Security issue

--- WeAds-FrontEnd/routes/userRoutes.js
+++ WeAds-FrontEnd/routes/userRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/home", function (req, res, next) {
-  res.render("index");
+  res.render("index", {
+    API_KEY: process.env.MAP_KEY,
+  });
 });
 
 module.exports = router;
