5b939b1e8818937e642c3a33582ddeb44c2ab0ef
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-14 19:24:33 +0700

    Added map to index view

--- WeAds-FrontEnd/app.js
+++ WeAds-FrontEnd/app.js
@@ -9,11 +9,17 @@ const userRoutes = require("./routes/userRoutes");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "public")));
 
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
 const MAP_KEY = process.env.MAP_KEY;
 
 app.listen(process.env.PORT);
 console.log("listening on port: " + process.env.PORT);
 
+app.get("/", (req, res) => {
+  res.redirect("/weads/home");
+});
 app.use("/weads", userRoutes);
