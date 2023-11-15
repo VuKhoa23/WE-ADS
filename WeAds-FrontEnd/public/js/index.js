253f730405580ddf25b1874b75b4c99427060c63
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-15 09:00:39 +0700

    Added sample markers

--- WeAds-FrontEnd/public/js/index.js
+++ WeAds-FrontEnd/public/js/index.js
@@ -2,6 +2,27 @@
 let map;
 let marker;
 
+const markers = [
+  {
+    address: "Pho Di Bo Nguyen Hue",
+    description: "Quang cao tren pho di bo",
+  },
+  {
+    address: "Cau Ba Son Quan 1",
+    description: "Quang cao tren cau Ba Son",
+  },
+];
+
+const markerIcon =
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fb/Map_pin_icon_green.svg/800px-Map_pin_icon_green.svg.png";
+
+const { Size } = await google.maps.importLibrary("core");
+
+const icon = {
+  url: markerIcon, // url
+  scaledSize: new Size(50, 50), // scaled size
+};
+
 async function initMap() {
   const response = await fetch(
     "https://maps.googleapis.com/maps/api/geocode/json?" +
@@ -33,9 +54,31 @@ async function initMap() {
   });
 }
 
+async function initMarkers() {
+  const { Marker } = await google.maps.importLibrary("marker");
+  markers.forEach(async (mark) => {
+    const response = await fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?" +
+        new URLSearchParams({
+          address: mark.address,
+          key: API_KEY,
+        })
+    );
+    const result = await response.json();
+    const position = result.results[0].geometry.location;
+    const marker = new Marker({
+      position: position,
+      map: map,
+      title: mark.address,
+      icon: icon,
+    });
+  });
+}
+
 // event handlers
-$(document).ready(function () {
-  initMap();
+$(document).ready(async function () {
+  await initMap();
+  await initMarkers();
   $("#search-btn").click(async function (event) {
     const response = await fetch(
       "https://maps.googleapis.com/maps/api/geocode/json?" +
