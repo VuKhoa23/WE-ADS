5a16afa5587bf27c85141fbb40b49aa6b278c7b7
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-15 07:58:07 +0700

    Finished location search with geocoding

--- WeAds-FrontEnd/public/js/index.js
+++ WeAds-FrontEnd/public/js/index.js
@@ -1,9 +1,18 @@
 // Initialize and add the map
 let map;
+let marker;
 
 async function initMap() {
+  const response = await fetch(
+    "https://maps.googleapis.com/maps/api/geocode/json?" +
+      new URLSearchParams({
+        address: "Bitexco Quan 1",
+        key: API_KEY,
+      })
+  );
+  const result = await response.json();
   // The location of Uluru
-  const position = { lat: -25.344, lng: 131.031 };
+  const position = result.results[0].geometry.location;
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
@@ -11,17 +20,41 @@ async function initMap() {
 
   // The map, centered at Uluru
   map = new Map(document.getElementById("map"), {
-    zoom: 10,
+    zoom: 15,
     center: position,
-    mapId: "DEMO_MAP_ID",
+    mapId: "53fd56e871556f80",
   });
 
   // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerElement({
+  marker = new AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "You are here",
   });
 }
 
-initMap();
+// event handlers
+$(document).ready(function () {
+  initMap();
+  $("#search-btn").click(async function (event) {
+    const response = await fetch(
+      "https://maps.googleapis.com/maps/api/geocode/json?" +
+        new URLSearchParams({
+          address: $("#text-field").val(),
+          key: API_KEY,
+        })
+    );
+
+    const result = await response.json();
+    console.log(result);
+    // The location of Uluru
+    if (result.results.length > 0) {
+      const position = result.results[0].geometry.location;
+      const latlng = new google.maps.LatLng(position.lat, position.lng);
+      map.setCenter(position);
+      marker.position = latlng;
+    } else {
+      alert("Location ko tim thay");
+    }
+  });
+});
