603b1147c6bd493f68d9112a7544305072dbc98c
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-18 20:30:46 +0700

    migrate to mapbox, finished geocoding with search box

--- WeAds-FrontEnd/app.js
+++ WeAds-FrontEnd/app.js
@@ -9,7 +9,6 @@ const userRoutes = require("./routes/userRoutes");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
