1f5733cab546904895b483f05692ff9de7a60d35
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-27 18:56:42 +0700

    Finished testing for WYSWYG

--- WeAds-BackEnd/controller/reportController.js
+++ WeAds-BackEnd/controller/reportController.js
@@ -1,18 +1,26 @@
-const Report = require('../model/reportSchema');
-var ObjectId = (require('mongoose').Types.ObjectId);
+const Report = require("../model/reportSchema");
+var ObjectId = require("mongoose").Types.ObjectId;
 
 //create new report
 module.exports.createReport = async (req, res, next) => {
-  const { name, reportType, email, phone, content, address} = req.body;
+  const { name, reportType, email, phone, content, address } = req.body;
   let state = false;
 
   try {
-    const report = await Report.create({ name, reportType, email, phone, content, address, state });
-    res.status(201).json({success: true});
-  }
-  catch (err) {
+    const report = await Report.create({
+      name,
+      reportType,
+      email,
+      phone,
+      content,
+      address,
+      state,
+    });
+    console.log(content);
+    res.status(201).json({ success: true });
+  } catch (err) {
     console.error(err.message);
-    res.status(400).json({success: false, message: err.message});
+    res.status(400).json({ success: false, message: err.message });
   }
 };
 
@@ -21,40 +29,42 @@ module.exports.getAllReports = async (req, res, next) => {
     const reports = await Report.find({});
 
     //get create time from document and add it to the results
-    const results = reports.map(report => {
+    const results = reports.map((report) => {
       const timeStamp = new Date(report.createdAt);
       return {
         ...report._doc,
-        createTime: `${timeStamp.getDate()}/${timeStamp.getMonth() + 1}/${timeStamp.getFullYear()}`
-      }
+        createTime: `${timeStamp.getDate()}/${
+          timeStamp.getMonth() + 1
+        }/${timeStamp.getFullYear()}`,
+      };
     });
-    res.status(200).json({success: true, reports: results});
-  }
-  catch (err) {
+    res.status(200).json({ success: true, reports: results });
+  } catch (err) {
     console.error(err.message);
-    res.status(500).json({success: false, message: err.message});
+    res.status(500).json({ success: false, message: err.message });
   }
 };
 
 //update report state
 module.exports.updateState = async (req, res, next) => {
   try {
-    const { id , state, information } =  req.body;
+    const { id, state, information } = req.body;
     console.log(id, state, information);
 
     if (!information || information.length === 0) {
-      res.status(400).json({success: false, message: "Update information is missing"})
+      res
+        .status(400)
+        .json({ success: false, message: "Update information is missing" });
     }
 
     let doc = await Report.findOne({ _id: new ObjectId(id) });
     doc.state = state;
     doc.information = information;
-    
+
     await doc.save();
-    res.status(200).json({success: true});
-  }
-  catch (err) {
+    res.status(200).json({ success: true });
+  } catch (err) {
     console.error(err.message);
-    res.status(500).json({success: false, message: err.message});
+    res.status(500).json({ success: false, message: err.message });
   }
 };
--- WeAds-BackEnd/model/reportSchema.js
+++ WeAds-BackEnd/model/reportSchema.js
@@ -1,41 +1,35 @@
-const mongoose = require('mongoose'); 
+const mongoose = require("mongoose");
 
-const reportSchema = mongoose.Schema({
+const reportSchema = mongoose.Schema(
+  {
     name: {
       type: String,
-      required: [true, 'Plank reporter name']
     },
     address: {
       type: String,
-      required: [true, 'Missing address information']
     },
     reportType: {
       type: String,
-      required: [true, 'Unkouwn reason to report']
     },
     email: {
       type: String,
-      required: [true, 'Reporter email is missing']
     },
     phone: {
       type: String,
-      required: [true, 'Reporter phone numer is missing']
     },
     content: {
       type: String,
-      required: [true, 'Plank report content']
     },
     state: {
       type: Boolean,
-      required: true
     },
     information: {
-      type: String
+      type: String,
     },
   },
-  {timestamps: true}
-)
+  { timestamps: true }
+);
 
-const Report = mongoose.model('report', reportSchema);
+const Report = mongoose.model("report", reportSchema);
 
 module.exports = Report;
