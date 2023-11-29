ddf5409f047263c5a82352685735c549e3523e4d
VuKhoa23 <vuanhkhoa007@gmail.com>
2023-11-29 11:13:35 +0700

    Finished markers clustering.

---
+++ WeAds-FrontEnd/public/js/geojson.js
@@ -0,0 +1,56 @@
+const geojson = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.7221713156813, 10.794929507221397],
+      },
+      properties: {
+        adType: "Trụ treo băng rôn dọc",
+        adScale: "2.5m x 5.4m",
+        adName: "Landmark 81",
+        adPlanned: 1,
+        title: "Adam",
+        "marker-color": "#FC5C53",
+        "marker-size": "large",
+        "marker-symbol": "rocket",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.71841681609948, 10.786080361035175],
+      },
+      properties: {
+        adType: "Trụ treo băng rôn dọc",
+        adScale: "2.5m x 5.4m",
+        adName: "Cầu Thủ Thiêm",
+        adPlanned: 0,
+        title: "Adam",
+        "marker-color": "#FC5C53",
+        "marker-size": "large",
+        "marker-symbol": "rocket",
+      },
+    },
+    {
+      type: "Feature",
+      geometry: {
+        type: "Point",
+        coordinates: [106.71136034376843, 10.792355602618315],
+      },
+      properties: {
+        adType: "Trụ treo băng rôn dọc",
+        adScale: "2.5m x 5.4m",
+        adName: "Đại học tôn đức thắng",
+        adPlanned: 0,
+        title: "Adam",
+        "marker-color": "#FC5C53",
+        "marker-size": "large",
+        "marker-symbol": "rocket",
+      },
+    },
+  ],
+};
