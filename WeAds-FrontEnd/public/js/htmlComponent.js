2ac16e7b036e5b11727da22f5d5fb005be6267fc
VuKhoa23 <vuanhkhoa007@gmail.com>
2024-01-09 21:58:37 +0700

    Checkpoint

--- WeAds-FrontEnd/public/js/htmlComponent.js
+++ WeAds-FrontEnd/public/js/htmlComponent.js
@@ -63,6 +63,25 @@ function warnAlert(name, address, ward, district, coordinates) {
   `;
 }
 
+function unpersistedAlert(name, address, id){
+  return `
+    <div class="alert alert-success alert-dismissible fade show m-auto mb-2 mt-1 row" role="alert" style="font-size: 13px; width: 90%">
+      <i class="fa-solid fa-check col-3"></i>
+      <div class="col-9">
+        Thông tin địa điểm
+        <hr>
+        <strong>${name}</strong> <br>
+        ${address}
+      </div>
+      <button type="button" class="btn-close m-2" data-bs-dismiss="alert" aria-label="Close"></button>
+      <br>
+      <div class="d-flex justify-content-end mt-3">
+        <a class="link-secondary w-50" target="_blank" href="/weads/view-report/${id}"><button class="btn btn-primary" style="font-size: 12px">Xem lại báo cáo</button></a>
+      </div>
+    </div>
+  `;
+}
+
 function popUp(address, type, planned) {
   return `
   <div style="width: 15vw">
