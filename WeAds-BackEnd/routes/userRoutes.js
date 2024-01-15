d82910b8d4a9cad76802950e117d1e282bc2e439
Thuận Phát <phat06112003@gmail.com>
2024-01-15 19:33:32 +0700

    add edit profile functionality

--- WeAds-BackEnd/routes/userRoutes.js
+++ WeAds-BackEnd/routes/userRoutes.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const Officer = require('../model/officer');
+const ObjectId = require('mongoose').Types.ObjectId;
 
 router.get("/home", function (req, res, next) {
   let username = null
@@ -9,6 +11,10 @@ router.get("/home", function (req, res, next) {
   }
   if(res.locals.user){
     username = res.locals.user.username
+  }
+  else {
+    res.redirect('/weads/login');
+    return;
   }
 
   let role = null
@@ -23,6 +29,91 @@ router.get("/home", function (req, res, next) {
     ward: res.locals.user ? res.locals.user.ward : null,
     district: res.locals.user ? res.locals.user.district : null
   });
+});
+
+router.get('/user/edit-profile', async (req, res) => {
+  let officerId = null;
+  if(res.locals.user){
+    officerId = res.locals.user._id;
+  }
+  if (!officerId) {
+    res.redirect('/weads/home');
+    return;
+  }
+  
+  let username = null
+  if(res.locals.user){
+    username = res.locals.user.username
+  }
+
+  let role = null
+  if(res.locals.user){
+    role = res.locals.user.role
+  }
+  const user = await Officer.findOne({ _id: new ObjectId(officerId)});
+  res.render('editProfile', {
+    user,
+    username,
+    role
+  })
+});
+
+router.get('/user/edit-profile', async (req, res) => {
+  let officerId = null;
+  if(res.locals.user){
+    officerId = res.locals.user._id;
+  }
+  if (!officerId) {
+    res.redirect('/weads/home');
+    return;
+  }
+  
+  let username = null
+  if(res.locals.user){
+    username = res.locals.user.username
+  }
+
+  let role = null
+  if(res.locals.user){
+    role = res.locals.user.role
+  }
+  const user = await Officer.findOne({ _id: new ObjectId(officerId)});
+  res.render('editProfile', {
+    user,
+    username,
+    role
+  })
+});
+
+router.post('/user/edit-profile', async (req, res) => {
+  const { email, name, phone } = req.body;
+  const user_name = req.body.username;
+  let officerId = null;
+
+  if(res.locals.user){
+    officerId = res.locals.user._id;
+  }
+  if (!officerId) {
+    res.status(404).json({ success: false, error: "officer id missing" });
+    return;
+  }
+  
+  let username = null
+  if(res.locals.user){
+    username = res.locals.user.username
+  }
+
+  let role = null
+  if(res.locals.user){
+    role = res.locals.user.role
+  }
+  await Officer.updateOne({ _id: new ObjectId(officerId)}, {
+    email,
+    name, 
+    phone,
+    username: user_name
+  });
+  res.status(200).json({ success: true })
 });
 
 module.exports = router;
